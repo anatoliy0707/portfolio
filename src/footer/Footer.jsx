40555e146ed72ef2e902535e180a26a175c2f8e6
anatoliy0707 <anatoliy_sukhanov@mail.ru>
2022-01-25 19:36:42 +0500

    fixed footer

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,18 +1,20 @@
-import style from './Footer.module.css'
+import style from './Footer.module.scss'
 import styleContainer from './../common/styles/Container.module.css'
 
 export const Footer = () => {
     return (
         <div className={style.footerBlock}>
-            <div className={`${styleContainer.container} ${style.container}`}>
-                <h2 className={style.title}>Anatoliy Sukhanov</h2>
-                <div className={style.iconBlock}>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                    <div className={style.icon}></div>
-                </div>
-                <span className={style.footerText}>Ⓒ 2022 Все права защищены</span>
+            <div className={style.title}>
+                <h2>Anatoliy</h2>
+            </div>
+            <div className={style.iconBlock}>
+                <div className={style.icon}></div>
+                <div className={style.icon}></div>
+                <div className={style.icon}></div>
+                <div className={style.icon}></div>
+            </div>
+            <div className={style.footerText}>
+                <span>Ⓒ 2022 Все права защищены</span>
             </div>
         </div>
     )
