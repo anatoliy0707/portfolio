cffc40a8ac28863dc309e592d3e705643405e2a9
anatoliy0707 <anatoliy_sukhanov@mail.ru>
2022-01-26 13:04:38 +0500

    footer finished

--- src/footer/Footer.jsx
+++ src/footer/Footer.jsx
@@ -1,17 +1,38 @@
 import style from './Footer.module.scss'
-import styleContainer from './../common/styles/Container.module.css'
+import linkImg from "../assets/images/linkedin.png"
+import teleImg from "../assets/images/telegram.png"
+import mailImg from "../assets/images/email.png"
+import gitImg from "../assets/images/github.png"
+
 
 export const Footer = () => {
+
+    const linkedin = {
+        backgroundImage: 'url(' + linkImg + ')',
+    };
+
+    const telegramm = {
+        backgroundImage: 'url(' + teleImg + ')',
+    };
+
+    const email = {
+        backgroundImage: 'url(' + mailImg + ')',
+    };
+
+    const github = {
+        backgroundImage: 'url(' + gitImg + ')',
+    };
+
     return (
         <div className={style.footerBlock}>
             <div className={style.title}>
                 <h2>Anatoliy</h2>
             </div>
             <div className={style.iconBlock}>
-                <div className={style.icon}></div>
-                <div className={style.icon}></div>
-                <div className={style.icon}></div>
-                <div className={style.icon}></div>
+                <div className={style.icon} style={linkedin}></div>
+                <div className={style.icon} style={telegramm}></div>
+                <div className={style.icon} style={email}></div>
+                <div className={style.icon} style={github}></div>
             </div>
             <div className={style.footerText}>
                 <span>Ⓒ 2022 Все права защищены</span>
